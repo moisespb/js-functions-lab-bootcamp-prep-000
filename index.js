28ec1e02a150f449d9ae3980e96db2304fc4ee6f
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-07-24 06:10:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+ // write your code below!
+function happyHolidays(){
+  return "happyHolidays"
+}
+
+function happyHolidaysTo(name){
+return  `Happy holidays, ${name}!` 
+
+}
+
+
+function happyHolidaysTo(name, holiday){
+return  `Happy ${holiday}, ${name}!` 
+
+}
+
+function holidayCountdown(days, holiday){
+`It's ${days} days until ${holiday}!`
+
+}
